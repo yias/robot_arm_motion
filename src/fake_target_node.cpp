29912dc0dbfb59fd5e60e3f465c7355ff8862a3c
yias <iasonbatz@gmail.com>
2020-12-02 22:42:56 +0100

    reading fake target and obstacles from yaml file

--- src/fake_target_node.cpp
+++ src/fake_target_node.cpp
@@ -1,9 +1,12 @@
 
+#include <string>
+#include <vector>
+#include <iostream>
+
 #include "ros/ros.h"
 
 #include <geometry_msgs/PoseStamped.h>
 #include "sensor_msgs/JointState.h"
-
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Quaternion.h"
@@ -11,6 +14,214 @@
 
 #include <robot_arm_motion/obstacle_msg.h>
 
+
+int parse_obs_params(std::string position_param, 
+                        std::string orientation_param, 
+                        std::string alpha_param, 
+                        std::string power_param, 
+                        std::vector<visualization_msgs::Marker> *obs_markers, 
+                        robot_arm_motion::obstacle_msg *obst_msg){
+
+
+    // load ros-parameters related to the obstacles
+
+    // obstacles position
+    XmlRpc::XmlRpcValue obstacles_pos_param;
+    ros::param::get("/target_obstacles_sim/" + position_param, obstacles_pos_param);
+    ROS_ASSERT(obstacles_pos_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+    // obstacles orientation
+    XmlRpc::XmlRpcValue obstacles_orient_param;
+    ros::param::get("/target_obstacles_sim/" + orientation_param, obstacles_orient_param);
+    ROS_ASSERT(obstacles_orient_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+    // obstacles alpha parameters
+    XmlRpc::XmlRpcValue obstacles_alpha_param;
+    ros::param::get("/target_obstacles_sim/" + alpha_param, obstacles_alpha_param);
+    ROS_ASSERT(obstacles_alpha_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+    // obstacles power parameters
+    XmlRpc::XmlRpcValue obstacles_power_param;
+    ros::param::get("/target_obstacles_sim/" + power_param, obstacles_power_param);
+    ROS_ASSERT(obstacles_power_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+    // check that all the parameters have the same length (as the number of obstacles)
+    size_t nb_obstacles = obstacles_pos_param.size();
+    if (obstacles_orient_param.size() != nb_obstacles){
+        std::cout << "[obstacle-target simulation] The number of obstacles in position parameter (rows) does not match the number of rows of the orientation parameters" << std::endl;
+        return -1;
+    }
+    
+    if (obstacles_alpha_param.size() != nb_obstacles){
+        std::cout << "[obstacle-target simulation] The number of obstacles in position parameter (rows) does not match the number of rows of the alpha parameters" << std::endl;
+        return -2;
+    }
+
+    if (obstacles_power_param.size() != nb_obstacles){
+        std::cout << "[obstacle-target simulation] The number of obstacles in position parameter (rows) does not match the number of rows of the power parameters" << std::endl;
+        return -3;
+    }
+
+    // setting the size of the obsacles in the obstacle message
+    obst_msg->obstacles.resize(nb_obstacles);
+
+    // parsing the parameters to the corresponding messages for each obstacle
+    for (int32_t i = 0; i < nb_obstacles; ++i) {
+        visualization_msgs::Marker obs_marker;
+        obs_marker.header.frame_id = "/world";
+        obs_marker.header.stamp = ros::Time();
+        obs_marker.ns = "obstacle_";                             // setting the name-space
+        obs_marker.id = i;                                       // setting the id of the marker
+        obs_marker.type = visualization_msgs::Marker::SPHERE;    // setting the shape of the marker
+        obs_marker.action = visualization_msgs::Marker::ADD;     // add this marker to the existing ones
+        
+        // parsing the position
+        if (obstacles_pos_param[i].size() != 3){
+            std::cout << "[obstacle-target simulation] The position coordinates for the " << i << " obstacle are not equal to 3" << std::endl;
+            return - 1 - i*10;
+        }
+        // to the marker message
+        obs_marker.pose.position.x = static_cast<double>(obstacles_pos_param[i][0]);
+        obs_marker.pose.position.y = static_cast<double>(obstacles_pos_param[i][1]);
+        obs_marker.pose.position.z = static_cast<double>(obstacles_pos_param[i][2]);
+
+
+        // to the obstacle message
+        obst_msg->obstacles[i].pose.position.x = obs_marker.pose.position.x;
+        obst_msg->obstacles[i].pose.position.y = obs_marker.pose.position.y;
+        obst_msg->obstacles[i].pose.position.z = obs_marker.pose.position.z;
+
+        // parsing the orientation
+        if (obstacles_orient_param[i].size() != 4){
+            std::cout << "[obstacle-target simulation] The orientation coordinates for the " << i << " obstacle are not equal to 4" << std::endl;
+            return - 2 - i*10;
+        }
+        //  to the marker message
+        obs_marker.pose.orientation.x = static_cast<double>(obstacles_orient_param[i][1]);
+        obs_marker.pose.orientation.y = static_cast<double>(obstacles_orient_param[i][2]);
+        obs_marker.pose.orientation.z = static_cast<double>(obstacles_orient_param[i][3]);
+        obs_marker.pose.orientation.w = static_cast<double>(obstacles_orient_param[i][0]);
+
+        // to the obstacle message
+        obst_msg->obstacles[i].pose.orientation.w = obs_marker.pose.orientation.w;
+        obst_msg->obstacles[i].pose.orientation.x = obs_marker.pose.orientation.x;
+        obst_msg->obstacles[i].pose.orientation.y = obs_marker.pose.orientation.y;
+        obst_msg->obstacles[i].pose.orientation.z = obs_marker.pose.orientation.z;
+
+        // parsing the alpha parameters 
+        if (obstacles_alpha_param[i].size() != 3){
+            std::cout << "[obstacle-target simulation] The alpha parameters for the " << i << " obstacle are not equal to 3" << std::endl;
+            return - 3 - i*10;
+        }
+        // to the marker message
+        obs_marker.scale.x = static_cast<double>(obstacles_alpha_param[i][0]);
+        obs_marker.scale.y = static_cast<double>(obstacles_alpha_param[i][1]);
+        obs_marker.scale.z = static_cast<double>(obstacles_alpha_param[i][2]);
+
+        // to the obstacle message
+        obst_msg->obstacles[i].alpha[0] = obs_marker.scale.x;
+        obst_msg->obstacles[i].alpha[1] = obs_marker.scale.y;
+        obst_msg->obstacles[i].alpha[2] = obs_marker.scale.z;
+
+        // setting the color (rbg) and the opacity (a) of the obstacle
+        obs_marker.color.a = 0.7;
+        obs_marker.color.r = 0.0;
+        obs_marker.color.g = 1.0;
+        obs_marker.color.b = 0.0;
+        obs_marker.lifetime = ros::Duration();
+        
+        obs_markers->push_back(obs_marker);
+
+        // parsing the alpha parameters 
+        if (obstacles_power_param[i].size() != 3){
+            std::cout << "[obstacle-target simulation] The power parameters for the " << i << " obstacle are not equal to 3" << std::endl;
+            return - 4 - i*10;
+        }
+        // to the obstacle message
+        obst_msg->obstacles[i].power_terms[0] = static_cast<double>(obstacles_power_param[i][0]);
+        obst_msg->obstacles[i].power_terms[1] = static_cast<double>(obstacles_power_param[i][1]);
+        obst_msg->obstacles[i].power_terms[2] = static_cast<double>(obstacles_power_param[i][2]);
+
+    }
+
+    return 0;
+}
+
+int parse_target_params(std::string position_param, 
+                        std::string orientation_param,  
+                        visualization_msgs::Marker *tgt_marker,
+                        geometry_msgs::Pose *tgt_msg){
+
+
+    // load ros-parameters related to the obstacles
+
+    // target position
+    XmlRpc::XmlRpcValue target_pos_param;
+    ros::param::get("/target_obstacles_sim/" + position_param, target_pos_param);
+    ROS_ASSERT(target_pos_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+    if (target_pos_param[0].getType() == XmlRpc::XmlRpcValue::TypeArray){
+        std::cout << "[obstacle-target simulation] Multiple targets are not supported. The target position and orientation should contain only one row." << std::endl;
+        return -1;
+    }
+
+    // target orientation
+    XmlRpc::XmlRpcValue target_orient_param;
+    ros::param::get("/target_obstacles_sim/" + orientation_param, target_orient_param);
+    ROS_ASSERT(target_orient_param.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+
+    tgt_marker->header.frame_id = "/world";
+    tgt_marker->header.stamp = ros::Time();
+    tgt_marker->ns = "target_marker_";
+    tgt_marker->id = 0;
+    tgt_marker->type = visualization_msgs::Marker::CYLINDER;
+    tgt_marker->action = visualization_msgs::Marker::ADD;
+
+    // parsing the position
+    if (target_pos_param.size() != 3){
+        std::cout << "[obstacle-target simulation] The position coordinates of the target are not equal to 3" << std::endl;
+        return -2;
+    }
+
+    // to the message of the target
+    tgt_msg->position.x = static_cast<double>(target_pos_param[0]);
+    tgt_msg->position.y = static_cast<double>(target_pos_param[1]);
+    tgt_msg->position.z = static_cast<double>(target_pos_param[2]);
+
+    // to the message of the marker
+    tgt_marker->pose.position.x = tgt_msg->position.x;
+    tgt_marker->pose.position.y = tgt_msg->position.y;
+    tgt_marker->pose.position.z = tgt_msg->position.z;
+
+    // parsing the orientation
+    if (target_orient_param.size() != 4){
+        std::cout << "[obstacle-target simulation] The orientation coordinates of the target are not equal to 4" << std::endl;
+        return -3;
+    }
+
+    tgt_msg->orientation.w = static_cast<double>(target_orient_param[0]); 
+    tgt_msg->orientation.x = static_cast<double>(target_orient_param[1]);
+    tgt_msg->orientation.y = static_cast<double>(target_orient_param[2]);
+    tgt_msg->orientation.z = static_cast<double>(target_orient_param[3]);
+
+    tgt_marker->pose.orientation.x = tgt_msg->orientation.x;
+    tgt_marker->pose.orientation.y = tgt_msg->orientation.y;
+    tgt_marker->pose.orientation.z = tgt_msg->orientation.z;
+    tgt_marker->pose.orientation.w = tgt_msg->orientation.w;
+    
+    // setting the scale, opacity and color of the marker of the target
+    tgt_marker->scale.x = 0.07;
+    tgt_marker->scale.y = 0.07;
+    tgt_marker->scale.z = 0.01;
+    tgt_marker->color.a = 0.9;
+    tgt_marker->color.r = 0.0;
+    tgt_marker->color.g = 0.4;
+    tgt_marker->color.b = 0.9;
+    tgt_marker->lifetime = ros::Duration();
+
+    return 0;
+}
 
 
 int main(int argc, char **argv)
@@ -33,146 +244,35 @@ int main(int argc, char **argv)
     // define ros-message for the obstacles
     robot_arm_motion::obstacle_msg obst_msg;
 
-    // define 2 obstacles
-
-    obst_msg.obstacles.resize(2);
-
-    // define the position and orienation of the first obstacle
-    obst_msg.obstacles[0].pose.position.x = -0.2f;
-    obst_msg.obstacles[0].pose.position.y = -0.4f;
-    obst_msg.obstacles[0].pose.position.z = 0.2f;
-
-    obst_msg.obstacles[0].pose.orientation.w = 0.0f;
-    obst_msg.obstacles[0].pose.orientation.x = 0.0f;
-    obst_msg.obstacles[0].pose.orientation.y = 1.0f;
-    obst_msg.obstacles[0].pose.orientation.z = 0.0f;
-
-    // define the axis lengths and the power terms of the first obstacle
-    obst_msg.obstacles[0].alpha[0] = 0.1f;
-    obst_msg.obstacles[0].alpha[1] = 0.2f;
-    obst_msg.obstacles[0].alpha[2] = 0.1f;
-
-    obst_msg.obstacles[0].power_terms[0] = 1.0f;
-    obst_msg.obstacles[0].power_terms[1] = 1.0f;
-    obst_msg.obstacles[0].power_terms[2] = 1.0f;
-
-
-    // for visualizing the first obstacle
-    visualization_msgs::Marker obs_marker0;
-    obs_marker0.header.frame_id = "/world";
-    obs_marker0.header.stamp = ros::Time();
-    obs_marker0.ns = "obstacle_";
-    obs_marker0.id = 0;
-    obs_marker0.type = visualization_msgs::Marker::SPHERE;
-    obs_marker0.action = visualization_msgs::Marker::ADD;
-    obs_marker0.pose.position.x = obst_msg.obstacles[0].pose.position.x;
-    obs_marker0.pose.position.y = obst_msg.obstacles[0].pose.position.y;
-    obs_marker0.pose.position.z = obst_msg.obstacles[0].pose.position.z;
-    obs_marker0.pose.orientation.x = obst_msg.obstacles[0].pose.orientation.x;
-    obs_marker0.pose.orientation.y = obst_msg.obstacles[0].pose.orientation.y;
-    obs_marker0.pose.orientation.z = obst_msg.obstacles[0].pose.orientation.z;
-    obs_marker0.pose.orientation.w = obst_msg.obstacles[0].pose.orientation.w;
-    obs_marker0.scale.x = obst_msg.obstacles[0].alpha[0];
-    obs_marker0.scale.y = obst_msg.obstacles[0].alpha[1];
-    obs_marker0.scale.z = obst_msg.obstacles[0].alpha[2];
-    obs_marker0.color.a = 0.7;
-    obs_marker0.color.r = 0.0;
-    obs_marker0.color.g = 1.0;
-    obs_marker0.color.b = 0.0;
-    obs_marker0.lifetime = ros::Duration();
-
-
-    // similarly define the position, orientation, alpha and power terms for the second obstacle
-
-    obst_msg.obstacles[1].pose.position.x = 0.2f;
-    obst_msg.obstacles[1].pose.position.y = -0.4f;
-    obst_msg.obstacles[1].pose.position.z = 0.2f;
-
-    obst_msg.obstacles[1].pose.orientation.w = 0.0f;
-    obst_msg.obstacles[1].pose.orientation.x = 0.0f;
-    obst_msg.obstacles[1].pose.orientation.y = 1.0f;
-    obst_msg.obstacles[1].pose.orientation.z = 0.0f;
-
-
-    obst_msg.obstacles[1].alpha[0] = 0.1f;
-    obst_msg.obstacles[1].alpha[1] = 0.1f;
-    obst_msg.obstacles[1].alpha[2] = 0.3f;
-
-    obst_msg.obstacles[1].power_terms[0] = 1.0f;
-    obst_msg.obstacles[1].power_terms[1] = 1.0f;
-    obst_msg.obstacles[1].power_terms[2] = 1.0f;
-
-
-    // for visualizing the second obstacle
-    visualization_msgs::Marker obs_marker1;
-    obs_marker1.header.frame_id = "/world";
-    obs_marker1.header.stamp = ros::Time();
-    obs_marker1.ns = "obstacle_";
-    obs_marker1.id = 1;
-    obs_marker1.type = visualization_msgs::Marker::SPHERE;
-    obs_marker1.action = visualization_msgs::Marker::ADD;
-    obs_marker1.pose.position.x = obst_msg.obstacles[1].pose.position.x;
-    obs_marker1.pose.position.y = obst_msg.obstacles[1].pose.position.y;
-    obs_marker1.pose.position.z = obst_msg.obstacles[1].pose.position.z;
-    obs_marker1.pose.orientation.x = obst_msg.obstacles[1].pose.orientation.x;
-    obs_marker1.pose.orientation.y = obst_msg.obstacles[1].pose.orientation.y;
-    obs_marker1.pose.orientation.z = obst_msg.obstacles[1].pose.orientation.z;
-    obs_marker1.pose.orientation.w = obst_msg.obstacles[1].pose.orientation.w;
-    obs_marker1.scale.x = obst_msg.obstacles[1].alpha[0];
-    obs_marker1.scale.y = obst_msg.obstacles[1].alpha[1];
-    obs_marker1.scale.z = obst_msg.obstacles[1].alpha[2];
-    obs_marker1.color.a = 0.7;
-    obs_marker1.color.r = 0.0;
-    obs_marker1.color.g = 1.0;
-    obs_marker1.color.b = 0.0;
-    obs_marker1.lifetime = ros::Duration();
-
+    // define vector of marker messages for the visualization of the obstacles
+    std::vector< visualization_msgs::Marker > obs_marker_msgs;
 
     // define the message for the target
     geometry_msgs::Pose target_msg;
 
-    // define the position and orienation for the target
-    target_msg.position.x = 0.5;
-    target_msg.position.y = -0.4;
-    target_msg.position.z = 0.22;
-
-    target_msg.orientation.w = 0.0; 
-    target_msg.orientation.x = 0.0;
-    target_msg.orientation.y = 1.0;
-    target_msg.orientation.z = 0.0;
-
-
-    // for visualizing the second obstacle
+    // define marker message for visualizing the target
     visualization_msgs::Marker target_marker;
-    target_marker.header.frame_id = "/world";
-    target_marker.header.stamp = ros::Time();
-    target_marker.ns = "target_marker_";
-    target_marker.id = 0;
-    target_marker.type = visualization_msgs::Marker::CYLINDER;
-    target_marker.action = visualization_msgs::Marker::ADD;
-    target_marker.pose.position.x = target_msg.position.x;
-    target_marker.pose.position.y = target_msg.position.y;
-    target_marker.pose.position.z = target_msg.position.z;
-    target_marker.pose.orientation.x = target_msg.orientation.x;
-    target_marker.pose.orientation.y = target_msg.orientation.y;
-    target_marker.pose.orientation.z = target_msg.orientation.z;
-    target_marker.pose.orientation.w = target_msg.orientation.w;
-    target_marker.scale.x = 0.07;
-    target_marker.scale.y = 0.07;
-    target_marker.scale.z = 0.01;
-    target_marker.color.a = 0.9;
-    target_marker.color.r = 0.0;
-    target_marker.color.g = 0.4;
-    target_marker.color.b = 0.9;
-    target_marker.lifetime = ros::Duration();
+
+
+    // fill the messeges with the parameters of the obstacles
+    if (parse_obs_params("ram_obstacles_position", "ram_obstacles_orientation", "ram_obstacles_alpha", "ram_obstacles_power_params", &obs_marker_msgs, &obst_msg) < 0){
+        std::cout << "[obstacle-target simulation] Correct the obstacles parameters in yaml file and launch again the node" << std::endl;
+        return -1;
+    }
+
+    // fill the messeges with the parameters of the target
+    if (parse_target_params("ram_target_position", "ram_target_orientation", &target_marker, &target_msg) < 0){
+        std::cout << "[obstacle-target simulation] Correct the obstacles parameters in yaml file and launch again the node" << std::endl;
+        return -2;
+    }
 
 
 	ros::Rate loop_rate(100);
 
-	int count = 0;
 
 	while (ros::ok()){
 
+        // publish the messages
 
         _pubTarget.publish(target_msg);
 
@@ -180,13 +280,10 @@ int main(int argc, char **argv)
 
         _pubOstacles.publish(obst_msg);
 
-        obs_marker0.header.stamp = ros::Time::now();
-
-        vis_pub.publish(obs_marker0);
-
-        obs_marker1.header.stamp = ros::Time::now();
-
-        vis_pub.publish(obs_marker1);
+        for(size_t i = 0; i < obs_marker_msgs.size(); i++){
+            obs_marker_msgs[i].header.stamp = ros::Time::now();
+            vis_pub.publish(obs_marker_msgs[i]);
+        }
 
         target_marker.header.stamp = ros::Time::now();
 
@@ -194,7 +291,7 @@ int main(int argc, char **argv)
 		
 		ros::spinOnce();
 		loop_rate.sleep();
-		++count;
+		// ++count;
 
 	}
 
